dc121dc107b71f241132d7d28383b8a968dc59c8
Landon <landinian@gmail.com>
2018-02-01 02:28:56 -0800

    Getting Started

---
+++ Operator.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int i0; int i1; double d0; double d1;
+
+	cin >> i0 >> i1 >> d0 >> d1;
+
+	cout << "i0 + i1 == " << i0 + i1 << '\n';
+	cout << "i0 + d0 == " << i0 + d0 << '\n';
+	cout << "d0 + d1 == " << d0 + d1 << '\n';
+
+
+
+	return 0;
+}
+
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Operators and Functions
 
 
-Class ID: 
+Class ID: Lillypup
 
 SRS ID: CST116SRS03
 
